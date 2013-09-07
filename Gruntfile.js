1225e56240f6e65eacc10c4907d92576e059235c
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-09-07 13:53:45 +0100

    include grunt for package and exclude bower_components

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    bower: {
+      install: {
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bower-task');
+
+  grunt.registerTask('default', ['bower']);
+};
