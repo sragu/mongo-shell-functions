9b5104b782a12ee00ded9309ec699c988d5ff9fc
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-09-10 01:43:49 +0100

    template project complete
    run > grunt
    
     to build single js at dist/app.js
     to configure multiple generated js check Grunt.js config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,24 @@
-'use strict';
 module.exports = function(grunt) {
 
   grunt.initConfig({
     bower: {
       install: {
+        options: {
+          targetDir: './vendor'
+        }
+      }
+    },
+    rig: {
+      compile: {        
+        files: {
+          'dist/app.js': ['src/app.js']
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-rigger');
 
-  grunt.registerTask('default', ['bower']);
+  grunt.registerTask('default', ['bower', 'rig']);
 };
---
+++ src/app.js
@@ -0,0 +1,5 @@
+//= ../vendor/lodash/lodash.compat.js
+
+// include any local library from ../lib
+
+// your functions here
