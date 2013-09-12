23abb3fda3e779a9b8536e66ff875bf9e7f5e480
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2013-09-12 21:51:06 +0100

    showing some lo-dash and util functions goodness

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
+// include vendor libraries
 //= ../vendor/lodash/lodash.compat.js
+//= ../vendor/mongo-uuid/mongo-uuid.js
 
 // include any local library from ../lib
 //= ../lib/common_functions.js
 
-// your functions here
-
+// your code here.
 db.test.remove()
 
-db.test.insert({
-    greet: 'Wassup!'
-});
+var _greet = function(msg) {
+    return {
+        greet: msg
+    };
+};
 
-db.test.insert({
-    greet: 'hola!'
-});
+var save = function(decorate, msg) {
+    db.test.insert(_greet(msg));
+};
+
+var saveGreetings = _.partial(save, _greet)
+
+_(['Wassup dude!', 'Hi mate!', 'Hi da!']).each(saveGreetings);
 
 db.test.find({}).forEach(print_json);
